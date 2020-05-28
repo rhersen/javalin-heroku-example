d41634a9cf31cf1b6c317d65daf1a6909cd03aa9
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-05-28 22:46:05 +0200

    get stats from strava.com/api

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,33 @@
 import io.javalin.Javalin
-import io.javalin.http.Context
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         val app = Javalin.create().start(herokuAssignedPort)
-        app.get("/") { ctx -> ctx.result("Hello World") }
+        app.get("/") { ctx -> ctx.result(athlete()) }
+    }
+
+    private fun athlete(): String {
+        val url = URL("https://www.strava.com/api/v3/athletes/13317026/stats")
+        val con: HttpURLConnection = url.openConnection() as HttpURLConnection
+        con.setRequestProperty("Authorization", "Bearer " + ProcessBuilder().environment()["BEARER"])
+        try {
+            val content = StringBuilder()
+            BufferedReader(InputStreamReader(con.inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    content.append(line)
+                    line = reader.readLine()
+                }
+                return content.toString()
+            }
+        } catch (e: Exception) {
+            return e.message ?: e.toString()
+        }
     }
 
     private val herokuAssignedPort: Int
